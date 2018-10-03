1762d73eb55c7d51f3ed20788ec7ed848422e435
wezarkeVo <wezarkevo@gmail.com>
2018-10-03 13:32:54 -0500

    Cambios temporales, se qitan en un tiempo

--- index.php
+++ index.php
@@ -1,116 +1,51 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Siena</title>
+        <meta name="description" content="Love profile card." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/projectName/frameworks/card/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+        <link rel="shortcut icon" type="image/png" href="/projectName/site_media/img/favicon/default.png" sizes="64x64"/>
+    </head>
+    <body class="demo-two">
+        <video autoplay loop class="video-background" muted plays-inline>
+            <source src="https://wireddots.metalogicssoft.com/themes/card/img/mountains.mp4" type="video/mp4">
+        </video>
+        <!--hero section-->
+        <section class="hero bg-indigo">
+            <div class="container">
+                <div class="row">
+                    <div class="card border-none col-lg-6 col-md-8 col-sm-10 mx-auto bg-texture">
+                        <div class="card-body text-white text-center py-4">
+                            <div class="mb-3">
+                                <img src="/projectName/frameworks/card/img/Siena_Eye_Dark.png" class="brand-logo img-fluid rounded-circle"/>
+                            </div>
+                            <h1 class="pt-4 text-white h2"> <span>Siena</span><span> Latam</span><br/></h1>
+                            <p class="mt-4 lead text-white">
+                                Tu entrenador Favorito
+                            </p>
+                            <p class="mt-2">
+                                <a href="https://github.com/LBSInteractive/htdocs" target="_blank"><em class="ion-social-github text-github-alt icon-sm mr-3"></em></a>
+                            </p>
+                            <div class="pt-3">
+                                <a href="#" class="btn btn-white-alt text-blue btn-lg">
+                                    Cargando...
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="col-sm-12 mt-5 footer">
+                        <p class="text-white small text-center">
+                            &copy; 2018
+                            <a href="#">Siena</a>. Designed by <a href="#">@Sienas</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-/****************************************************************************************************Notas/
-
-Bienvenido, aqui podras configurar tu core de direccionamiento para tu pagian web
-
-/Iniciemos con nombramiento de la carpeta
--Cambia el nombre de la carpeta ProjectName por la que una que este más acorde a tu gusto y al negocio
-Cambias el nombre de la carpeta /projectName/
-
-
--Ahora puedes realizar una primer petición sobre localhost:puerto /localhost:8080/ y podras ver la
-pantalla del generador, (El generador es la herramienta que te permitira crear templates HTML).
-
-
-Nota: Si deseas bloquear la navegación por tu arbol de trabajo, es decir, por medio de URLs, en ese caso
-puedes copiar en la rutas generales o especificas, el archivo index.php es decir este, y mantener el
-redireccionamiento al área de logeo.
-
-/****************************************************************************************************FIN Notas*/
-
-
-
-
-
-/****************************************************************************************************Router*/
-
-/* En esta área puedes realizar diferente redirecciones, tanto para redirect HTTP o HTTPS, o por lo
-*  contrario si deseas redireccionar al Generador.
-Copiar y pegar el header generico debajo del comentario token router
-*/
-
-/*Router Server HTTPS
-* Action: forzar direccionamiento por https
-*
-*     header("Location: https://dominio/projectName/...../Controller");
-*
-*/
-
-
-/*Router Server No-HTTP
-* Action: forzar direccionamiento por http
-*
-*     header("Location: http://dominio/projectName/..../Controller");
-*
-*/
-
-
-/*Router Server local
-* Action: direccionamiento por localhost
-*
-*     header("Location: /projectName/..../Controller");
-*
-*/
-
-
-/*Router Generador
-* Action: direccionamiento al generador
-*
-*
-*     header("Location: /projectName/core/generator_files/generator/generator_html.html");
-*
-*/
-
-
-                               /**token router**/
-header("Location: /projectName/core/generator_files/generators/generator_html.html");
-
-
-/****************************************************************************************************FIN Router*/
-
-
-
-
-
-/****************************************************************************************************Pruebas*/
-
-/* Aquí podras realizar test de prueba, donde podras evidenciar ejemplos como el error 404 y su pagina de
-redirección correspondiente.
-//Copia la linea header prefabricada, descomentala y copiala debajo de pruebas
-
-/*
-*  URL para pruebas
-*/
-
- /* Tipo: Error 404
- *  Action: direccionamiento al error 404 Apache por defecto
- *
- *	  header("HTTP/1.0 404 Not Found");
- *
- */
-
-
-/*
-*  Envio de encabezados como Status de pruebas
- /
-
- /* Tipo: Error 404
- *  Action: direccionamiento al error 404 Apache por defecto
- *
- *	  http_response_code(NumeroStatus);
- *
- *Ejemplo
- *
- *    http_response_code(404);
- *
- */
-
-
-                            /*pruebas*/
- //http_response_code(200);
-
-/****************************************************************************************************FIN Pruebas*/
-
-
- ?>
+    </body>
+</html>
